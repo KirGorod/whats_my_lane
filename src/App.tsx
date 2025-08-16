37c1de675bb8624a6d34108d4e12118e0fc87809
KirGorod <kgorodisky@gmail.com>
2025-08-16 23:32:05 +0300

    scroll to top on page open

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
 import CompetitionPage from "./pages/CompetitionPage/CompetitionPage";
 import { Toaster } from "sonner";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import { AuthProvider } from "./context/AuthContext";
 import Header from "./components/main/Header";
 import ExercisesPage from "./pages/ExercisesPage/ExercisesPage";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <AuthProvider>
       <Router>
+        <ScrollToTop />
         <Header />
         <Routes>
           <Route path="/" element={<ExercisesPage />} />
